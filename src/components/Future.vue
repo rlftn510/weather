bcc1b95aa39ae678df753477c38850917a49d7b8
galgale <rlftn510@naver.com>
2019-07-11 20:36:19 +0900

    요일 객체 생성 하여 치환 undifind 에러 존재.

--- src/components/Future.vue
+++ src/components/Future.vue
@@ -2,10 +2,11 @@
   <div class="future_warp">
     <!-- 미래 -->
     <div>
-      <h2>오늘</h2>
+      <h2>오늘({{today[0].weekInfo}})</h2>
       <ul>
         <li v-for="(item, idx) in today" v-bind:key="idx">
           <span class="day1">{{item.dt_txt.split(' ')[1].split(':')[0]}} 시</span>
+          <!-- <span class="day1">{{item.times}} 시</span> -->
           <p><img v-bind:src="'http://openweathermap.org/img/w/'+item.weather[0].icon+'.png'"></p>
           <p>{{item.weather[0].main}}</p>
           <p>{{Math.floor(item.main.temp - 272)}}℃</p>
@@ -13,7 +14,7 @@
       </ul>
     </div>
     <div>
-      <h2>내일</h2>
+      <h2>내일({{tomorrow[0].weekInfo}})</h2>
       <ul>
         <li v-for="(item, idx) in tomorrow" v-bind:key="idx">
           <span class="day1">{{item.dt_txt.split(' ')[1].split(':')[0]}} 시</span>
@@ -24,7 +25,7 @@
       </ul>
     </div>
     <div>
-      <h2>모레</h2>
+      <h2>모레({{ttomorrow[0].weekInfo}})</h2>
       <ul>
         <li v-for="(item, idx) in ttomorrow" v-bind:key="idx">
           <span class="day1">{{item.dt_txt.split(' ')[1].split(':')[0]}} 시</span>
@@ -52,12 +53,10 @@ export default {
   created(){
     this.forcastApi()
       .then(() => {
-        console.log(this.arr)
         this.slideDay()
         console.log(this.today)
         console.log(this.tomorrow)
         console.log(this.ttomorrow)
-        console.log(this.tttomorrow)
       })
   },
   mounted(){
@@ -79,25 +78,35 @@ export default {
     slideDay(){
       const today = new Date().getDate()
       const tomorrow = today + 1
-      const ttomorrow = tomorrow + 1
-      const tttomorrow = ttomorrow + 1
-      console.log(today.time)
+      const ttomorrow = today + 2
+      const tttomorrow = today + 3
+      console.log(today)
       console.log(tomorrow)
       console.log(ttomorrow)
       console.log(tttomorrow)
+      const week = ['일','월','화','수','목','금','토']
       this.arr.forEach((el, idx) => {
-          const day = new Date(el.dt_txt.split(' ')[0]).getDate()
-          // const times = el.dt_txt.split(' ')[1].split(':')[0]
-          console.log(day)
-          if(day === today){
+          let day = el.dt_txt.split(' ')[0]
+          let dayInfo = new Date(day).getDate()
+          let weekInfo = week[new Date(day).getDay()]
+          let times = el.dt_txt.split(' ')[1].split(':')[0]
+          // console.log(day)
+          if(dayInfo === today){
             this.today.push(el)
-            // this.today.time.push(times)
-          } else if (day == tomorrow){
+            this.today[0]['times'] = times
+            this.today[0]['weekInfo'] = weekInfo
+          } else if (dayInfo == tomorrow){
             this.tomorrow.push(el)
-          } else if (day == ttomorrow){
+            // this.tomorrow[0]['times'] = times
+            this.tomorrow[0]['weekInfo'] = weekInfo
+          } else if (dayInfo == ttomorrow){
             this.ttomorrow.push(el)
-          } else if (day == tttomorrow){
+            // this.ttomorrow[0]['times'] = times
+            this.ttomorrow[0]['weekInfo'] = weekInfo
+          } else if (dayInfo == tttomorrow){
             this.tttomorrow.push(el)
+            // this.tttomorrow[0]['times'] = times
+            this.tttomorrow[0]['weekInfo'] = weekInfo
           }
         })
         
